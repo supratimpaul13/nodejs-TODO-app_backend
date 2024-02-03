83f8f2069cde5951c38d84d646c015c7ca1f3093
supratimpaul13 <supratimpaul13@gmail.com>
2024-02-03 19:16:14 +0530

    fixed database

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(
     cors({
         origin: [process.env.FRONTEND_URL],
-        methods: ["GET", "PUT", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
